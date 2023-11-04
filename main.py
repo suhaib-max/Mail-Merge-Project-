be40a67157fef57fa62017da742893a6b3284537
suhaib <suhaibvbm@gmail.com>
2023-11-04 15:51:42 +0530

    created new_letter removed existing name (placeholder) in model letter replaced list of name and stripped the name inorder to avoid space

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt") as names_file:   #opening names
     names = names_file.readlines()
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
     letter_content = letter_file.read()
-    print(letter_content)
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        print(new_letter)
 
 
