de5d9ffd3eaeff6420281602074a0b4a2b06182e
suhaib <suhaibvbm@gmail.com>
2023-11-04 15:43:17 +0530

    opened and reading letter file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-with open("./Input/Names/invited_names.txt") as names_file:
+
+with open("./Input/Names/invited_names.txt") as names_file:   #opening names
     names = names_file.readlines()
-    print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    print(letter_content)
+
 
